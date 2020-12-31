3162e7d74efd4faec028c05c33ddd3e6e81ddf3e
Kamil Rosinski <kamilrosiczka@gmail.com>
2020-12-31 13:33:18 +0100

    Fix milticlick bug

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 function circleClicked() {
-    showText(String.fromCodePoint(Math.floor(Math.random() * 32) + 128512));
+    const circle = document.getElementById('circle');
+    circle.disabled = true;
+    circle.innerText = String.fromCodePoint(Math.floor(Math.random() * 32) + 128512);
     setTimeout(showCircle, 1000);
 }
 
 function showCircle() {
-    showText('');
     const circle = document.getElementById('circle');
     circle.style.top = `${Math.random() * 100}%`;
     circle.style.left = `${Math.random() * 100}%`;
-    circle.classList.remove('hidden');
-
-}
-
-function showText(text) {
-    document.getElementById('circle-text').innerText = text;
+    circle.innerText = '';
+    circle.disabled = false;
 }
